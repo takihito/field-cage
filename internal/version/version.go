499730ef9f14a3b2fc1d9dc1e61ecf9e41e6dfb6
takeda akihito <takeda-akihito@kayac.com>
2026-08-26 20:35:37 +0900

    [tagpr] prepare for the next release

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,4 +4,4 @@ package version
 // Version is kept in sync with the most recent release tag by tagpr (see
 // the versionFile setting in .tagpr). Release builds override the value
 // baked into the binary via -ldflags "-X main.version=...".
-const Version = "0.1.3"
+const Version = "0.1.4"
